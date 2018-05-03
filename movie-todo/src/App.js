f0382417ce409ba3c18036a0ef55d5bf29cc6abd
marietrull <marietrull@gmail.com>
2018-05-03 14:26:36 -0500

    created app and setup login folder

---
+++ movie-todo/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+
+class App extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      isLogged: false
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ movie-todo/src/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Login extends Component {
+	render(){
+		return (
+			<div>
+			<h5> Username </h5>
+			<input type='text' />
+			</div>
+		)
+	}
+}
+
+export default Login;
