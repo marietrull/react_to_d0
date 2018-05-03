536c85d34ba05253419f1c2dfa5e760b31290681
marietrull <marietrull@gmail.com>
2018-05-03 15:29:25 -0500

    Created state in MovieContainer

--- movie-todo/src/MovieContainer/index.js
+++ movie-todo/src/MovieContainer/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class MovieContainer extends Component {
+	constructor (){
+		super();
+
+		this.state = {
+			movies: ['Young Frankenstein', 'Blazing Saddles', 'History of the World Part One', 'The Producers']
+		}
+	}
 	render () {
 		return (
 			<span> Hi {this.props.username}</span>
