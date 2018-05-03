84f39c2c9d126d5a572700260c20eb075f80f691
marietrull <marietrull@gmail.com>
2018-05-03 16:40:54 -0500

    wrote addMovie function and passed it down to CreateMovie as a prop - added onSubmit to form in CreateMovie - wrote updateMovie to collect user information

--- movie-todo/src/CreateMovie/index.js
+++ movie-todo/src/CreateMovie/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component} from 'react';
 
-const CreateMovie = (props) => {
+//'wrote addMovie function and passed it down to CreateMovie as a prop - added onSubmit to form in CreateMovie - wrote updateMovie to collect user information onChange'
 
+class CreateMovie extends Component {
+	constructor (){
+		super();
+		this.state = {
+			movie: ''
+		}
+	}
+	updateMovie = (e) => {
+		const movie = e.currentTarget.value;
+		this.setState({movie: movie})
+	}
+
+	render (){
+		console.log(this.state, ' state inCreateMovie')
 	return (
-		<form>
+		<form onSubmit={this.props.addMovie.bind(null, this.state.movie)}>
 			<label> 
 				Movie:
-				<input type="text" name="movie"/>
+				<input type="text" name="movie" onChange={this.updateMovie}/>
 			</label>
-			<input type="Submit" value="Submit"/>
+			<input type="Submit"/>
 		</form>
-
-
 		)
+	}
 }
 
 export default CreateMovie;
--- movie-todo/src/MovieContainer/index.js
+++ movie-todo/src/MovieContainer/index.js
@@ -10,15 +10,24 @@ class MovieContainer extends Component {
 			movies: ['Young Frankenstein', 'Blazing Saddles', 'History of the World Part One', 'The Producers']
 		}
 	}
+	addMovie = (movie, e) => {
+		//Stop page from refreshing every time the form is submitted
+		e.preventDefault();
+		console.log(movie, ' this is the move')
+		console.log(e.currentTarget, ' this is the currentTarget')
+		console.log(e.target, ' this is e.target')
+		this.setState({movies: [...this.state.movies, movie]})	
+	}
 	render () {
 		return (
 			<div>
 				<span> Hi {this.props.username}</span>
 				<Movie movies={this.state.movies}/>
-				<CreateMovie />
+				<CreateMovie addMovie={this.addMovie}/>
 			</div>
 			)
 	}
 }
+
 
 export default MovieContainer;
