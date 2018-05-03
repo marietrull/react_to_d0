8fb1e819b15673222dc7fa69cd38c353f7f5fb42
marietrull <marietrull@gmail.com>
2018-05-03 14:29:17 -0500

    Added movie container and imported to app.js

--- movie-todo/src/App.js
+++ movie-todo/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import MovieContainer from './MovieContainer';
 
 class App extends Component {
   constructor () {
---
+++ movie-todo/src/MovieContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MovieContainer extends Component {
+	render () {
+		return (
+			<span> This is the MovieContainer </span>
+			)
+	}
+}
+
+export default MovieContainer;
